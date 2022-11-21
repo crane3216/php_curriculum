a41385551f77374b70ca273f0ac51688d26a2e57
crane3216 <turumi3216@gmail.com>
2022-11-21 23:44:43 +0900

    課題提出

---
+++ 2-8/index.php
@@ -0,0 +1,8 @@
+<?php
+  $fruits = ["apple" => "といったらリンゴ","orange" => "といったらミカン","peach" => "といったらモモ"];
+
+  foreach ($fruits as $key => $value){
+    echo $key;
+    echo $value. '<br />';
+  }
+?>
