3dd873803b0fc3f5b71970177c66e797ea898ed9
crane3216 <turumi3216@gmail.com>
2022-11-24 20:01:01 +0900

    課題提出(再)

--- 2-7/index.php
+++ 2-7/index.php
@@ -2,6 +2,6 @@
   $colors = ["red" => "赤", "blue" => "青", "green" => "緑"];
   var_dump($colors);
   echo '<br />';
-  $colors = $colors + ["yellow" => "黄色"];
+  $colors["yellow"] = "黄色";
   var_dump($colors);
 ?>
